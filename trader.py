7f4285bac2369d866967f282f2c817ea48b41ff6
unknown <dudu@GLB-DV-10.trad.local>
2014-04-06 12:58:42 +0300

    Get Dudu a stub color server

--- trader.py
+++ trader.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 import xmlrpc.client
-msgserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6712')
-print("Connected to message server with functions: %s" % (', '.join(msgserver.system.listMethods())))
-
-#colorserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6711')
-#print(colorserver.getvoutcolor('1'))
-#print(colorserver.system.listMethods())
+colorserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6711')
+print("Connected to message server with functions: %s" % (', '.join(colorserver.system.listMethods())))
 
 try:
     while True:
-        cmd = input("\nType command ([P]ropose, [O]ffer, [I]nbox, [Q]uit)\n")
+        cmd = input("\nType command ([G]et vout color, [M]ake transaction, [Q]uit)\n")
         if 'q' == cmd.lower(): break
-        if 'i' == cmd.lower():
-            print(msgserver.listen(''))
+        if 'g' == cmd.lower():
+            print(colorserver.getvoutcolor(input("txhash:voutidx: ")))
             continue
-        if 'o' == cmd.lower():
-            print(msgserver.offer(input("\noffer\n")))
-            continue
-        if 'p' == cmd.lower():
-            print(msgserver.propose(input("\npropose\n")))
+        if 'm' == cmd.lower():
+            print(colorserver.makeconversion(input("convert: ")))
             continue
 except KeyboardInterrupt:
     from sys import exit
